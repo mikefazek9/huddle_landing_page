eb6bab2ab842dce9938b50eff7352bfa999dace1
michael fazekas <mike.fazek@gmail.com>
2023-04-04 16:25:54 -0500

    start up files

---
+++ src/layout/Hero.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Hero() {
+  return <div className="hero_main"></div>
+}
+
+export default Hero
---
+++ src/layout/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import huddle from './images/logo.svg'
+function Nav() {
+  return (
+    <div className="nav_menu">
+      <img src={huddle} alt="logo huddle" className="huddle_logo" />
+    </div>
+  )
+}
+
+export default Nav
